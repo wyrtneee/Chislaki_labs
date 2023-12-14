85cd8eabf45a6036d54e8beeca366173ff10cf97
wyrtneee <147799833+wyrtneee@users.noreply.github.com>
2023-12-15 01:29:17 +0300

    Update main.cpp

--- laba2k/main.cpp
+++ laba2k/main.cpp
@@ -2,19 +2,37 @@
 #include <math.h>
 #include <iomanip>
 #include <vector>
-#include "functions.hpp"
+#include "functions.h"
 using namespace std;
 
-                int main()
-                {
-                    setlocale(LC_ALL, "ru");
-                    double x1, x2;
-                    vector<double> answer(2);
-                    x1 = 0.5;
-                    x2 = 0.2;
-                  answer=Newton_method(x1, x2, 2);
-                  cout << endl;
-                  cout << "Ответ: ";
-                  print(answer, 2);
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    double x1, x2;
+    vector<double> answer(2);
+    x1 = 0.5;
+    x2 = 0.2;
+    answer = Newton_method(x1, x2, 2, 0);
+    cout << endl;
+    cout << "ŒÚ‚ÂÚ: ";
+    print(answer, 2);
+    cout << endl;
 
-                }
+    answer = Newton_method(x1, x2, 2, 0.01);
+    cout << endl;
+    cout << "ŒÚ‚ÂÚ: ";
+    print(answer, 2);
+    cout << endl;
+
+    answer = Newton_method(x1, x2, 2, 0.05);
+    cout << endl;
+    cout << "ŒÚ‚ÂÚ: ";
+    print(answer, 2);
+    cout << endl;
+
+    answer = Newton_method(x1, x2, 2, 0.1);
+    cout << endl;
+    cout << "ŒÚ‚ÂÚ: ";
+    print(answer, 2);
+    cout << endl;
+}
