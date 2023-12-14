f0876c130651e3e60b66b40e0ec8296930557771
wyrtneee <147799833+wyrtneee@users.noreply.github.com>
2023-12-15 01:28:22 +0300

    Update functions.hpp

--- laba2k/functions.hpp
+++ laba2k/functions.hpp
@@ -1,10 +1,3 @@
-//
-//  functions.hpp
-//  laba2k
-//
-//  Created by Ксения Герасименя on 7.12.23.
-//
-
 #ifndef functions_hpp
 #define functions_hpp
 #include <vector>
@@ -24,7 +17,7 @@ double df21(double x1, double x2);
 double df22(double x1, double x2);
 void print(const vector<vector<double>>& matrix, int m, int n);
 void print(const vector<double>& matrix, int m);
-vector<vector<double>> J( double x1, double x2);
+vector<vector<double>> J(double x1, double x2, double M);
 vector<double>  otric_vector(double x1, double x2);
 vector<double> Gauss_method(vector<vector<double>>& matrix1, int m, int n, int k);
-vector<double> Newton_method(double x1, double x2, int n);
+vector<double> Newton_method(double x1, double x2, int n, double M);
